ddc7cf3bfde0f0bc2fbf0f741ce2958788da303e
siddharth <siddharth481998@gmail.com>
2026-09-19 16:19:00 +0530

    third

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,3 +17,12 @@ provider "aws" {
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
